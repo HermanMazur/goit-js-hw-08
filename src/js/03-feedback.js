83b8151768dc31069cd5ca750a7813e17b91c9ca
color <95304946+HermanMazur@users.noreply.github.com>
2022-06-19 17:55:08 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
 
+import throttle from "lodash.throttle";
+
+const STORADE_KEY = "feedback-form-state";
+const formData = {};
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+    input: document.querySelector('.feedback-form input'),
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', onFormDataInput);
+populateFormData();
+
+function onFormDataInput(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+}
+
+
+function onFormDataInput(evt) {
+    
+}
