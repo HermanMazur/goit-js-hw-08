95c2e6562420024eab4012c2b0fb7fd8eeb9f445
color <95304946+HermanMazur@users.noreply.github.com>
2022-06-19 13:11:05 +0300

    2 t fixed getItem!!!

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,8 @@
-import player from "@vimeo/player";
+import Player from "@vimeo/player";
 import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('iframe');
-    const player = new npmPlayer(iframe);
+    const player = new Player(iframe);
 
     player.getVideoTitle().then(function(title) {});
         
@@ -14,8 +14,8 @@ const iframe = document.querySelector('iframe');
 player.on('timeupdate', throttle(onPlay, 1000));
 
 player
-    .setCurrentTime(localStorage.setItem('videoplayer-current-time', currentTime)).
-    then(function (seconds) { })
+    .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+    .then(function (seconds) {})
     .catch(function (error) {
         switch (error.name) {
             case 'RangeError':
