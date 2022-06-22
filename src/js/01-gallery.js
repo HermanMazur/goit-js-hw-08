096a15416032fd3b940adebdbcfad82ce7f4576b
color <95304946+HermanMazur@users.noreply.github.com>
2022-06-22 13:24:46 +0300

    <li> delete 1task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -12,11 +12,12 @@ listGalleryItems.insertAdjacentHTML("afterbegin", markup);
 function createGalleryMarkup(galleryItems) {
     return galleryItems
         .map(({ preview, original, description }) => {
-            return `<li>
+            return `
             <a class="gallery__item" href="${original}">
 <img class="gallery__image" src="${preview}" alt="${description}" />
-</a></li>`;
-        }).join("");
+</a>`;
+        })
+        .join("");
 }
 
 new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250, });
